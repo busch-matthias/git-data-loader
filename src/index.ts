e6af6437a96c674add0d0cd270cb44a2a7d12a8f
Matthias Busch <matthias@futureofvoice.com>
2019-01-05 02:15:43 +0100

    added stuff

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,26 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import * as Octokit from '@octokit/rest';
-
 import { config } from './environment'
 
 import { crawlRepositories, CrawlResult } from './repos'
 import { RepositoryModel, RepoIdentifier } from './model'
 import { showLimits } from './showlimit'
 import { Configuration } from 'tslint';
-import { consoleTestResultHandler } from 'tslint/lib/test';
-interface FullConfiguration {
+
+export interface FullConfiguration {
     outputFile: string;
     outputFolder: string;
     statiticsFile: string;
     inputFolder: string;
     inputFile: string;
     todoFile: string;
+    fishyFile: string;
+
+    shoudOverrideOutput: boolean
+    shouldConsoleLog: boolean;
+    flattenOutput: boolean;
+    skipTodo: boolean;
 }
 
 export interface Configuration extends Partial<FullConfiguration> { }
@@ -31,6 +36,12 @@ const DEFAULT_CONFIG: FullConfiguration = {
     inputFolder: 'repo_input',
     inputFile: 'repos.json',
     todoFile: 'todo.json',
+    fishyFile: 'naughtyRepos.json',
+
+    shoudOverrideOutput: false,
+    shouldConsoleLog: false,
+    flattenOutput: true,
+    skipTodo: false
 }
 
 const gitApi = new Octokit({
@@ -63,39 +74,54 @@ async function main() {
     }
 }
 export async function generateInputFile(gitApi: Octokit, config: Configuration = {}): Promise<void> {
-    let fullConfig = { ...DEFAULT_CONFIG, ...config }
+    let fullConfig = { ...DEFAULT_CONFIG, ...config };
+
+    console.log('here we go');
+    let response = await gitApi.repos.listPublic({ per_page: 100 });
+
 }
 export async function loadRepositories(config: Configuration = {}): Promise<CrawlResult> {
     let fullConfig = { ...DEFAULT_CONFIG, ...config }
 
-    const repoIds = await loadTodoList(fullConfig);
+    const repoIds = await loadRepoList(fullConfig);
 
-    const crawlResult = await crawlRepositories(repoIds, gitApi);
+    const crawlResult = await crawlRepositories(repoIds, gitApi, fullConfig);
 
     await saveResults(crawlResult, fullConfig);
-    
+
     if (!crawlResult.isFinished()) {
-        console.info("Cralw was not finished in one run")
+        console.info("Cralw was not finished in one run");
         await saveTodos(crawlResult, fullConfig);
+    }
+    if (crawlResult.hasFishyRepos()) {
+        console.info("<<====>>>>><<<<<<<<>>>>>>><<<<<<====>>");
+        console.info(`there were some fishy repos(${crawlResult.fhisyRepos.length}), we added them to the list`);
+        await saveFishy(crawlResult, fullConfig)
     }
     return crawlResult;
 }
 
-async function loadTodoList(config: FullConfiguration): Promise<Array<RepoIdentifier>> {
-    const inputPath :string = path.join(config.inputFolder, config.inputFile);
-    const todoPath :string = path.join(config.inputFolder, config.todoFile);
-    if (fs.existsSync(todoPath)) {
-        console.info(">>>ERROR! can not read from todo-File at the moment");
+async function loadRepoList(config: FullConfiguration): Promise<Array<RepoIdentifier>> {
+    const inputPath: string = path.join(config.inputFolder, config.inputFile);
+    const todoPath: string = path.join(config.inputFolder, config.todoFile);
+
+    let read = (path: string, ): Array<RepoIdentifier> => {
+        const input: Array<RepoIdentifier> = JSON.parse(fs.readFileSync(path).toString());
+        console.log("We have " + input.length + " repos to read");
+        return input;
+    }
+    if (fs.existsSync(todoPath) && !config.skipTodo) {
+        console.info("Will read from Todo");
+        return read(todoPath);
+
     } else {
         if (!fs.existsSync(inputPath)) {
             console.info(">>>ERROR! No input file specified");
         }
-        let content  = fs.readFileSync(inputPath);
-        const input: Array<RepoIdentifier> = JSON.parse(content.toString());
-        console.log("We have "+ input.length+" repos to read")
-        return input;
+
+        return read(inputPath);
     }
-    return [];
+
 }
 async function saveResults(cralwResult: CrawlResult, config: FullConfiguration): Promise<void> {
 
@@ -104,22 +130,45 @@ async function saveResults(cralwResult: CrawlResult, config: FullConfiguration):
     }
 
     const outputPath = path.join(config.outputFolder, config.outputFile);
-    const output = JSON.stringify(cralwResult.loadedRepos,null,2);
+
+    let loaded = cralwResult.loadedRepos;
+    if (!config.shoudOverrideOutput) {
+        if (fs.existsSync(outputPath)) {
+            let existingRepos: Array<RepositoryModel> = JSON.parse(fs.readFileSync(outputPath).toString());
+            console.log('loaded existing repos:  ' + existingRepos.length)
+            loaded= loaded.concat(existingRepos);
+        }
+    }
+
+    let output;
+    if (config.flattenOutput) {
+        output = JSON.stringify(loaded);
+    } else {
+        output = JSON.stringify(loaded, null, 2);
+    }
 
     console.info("<<====>>>>><<<<<<<<>>>>>>><<<<<<====>>")
-    console.info("we saved " + cralwResult.loadedRepos.length + " repos");
+    console.info("we saved " + loaded.length + " repos");
 
-    await fs.promises.appendFile(outputPath, output);
+    await fs.promises.writeFile(outputPath, output);
 }
 
-async function saveTodos(cralwResult: CrawlResult, config: FullConfiguration): Promise<void>{
+async function saveTodos(cralwResult: CrawlResult, config: FullConfiguration): Promise<void> {
     if (!fs.existsSync(config.outputFolder)) {
         fs.mkdirSync(config.outputFolder)
     }
-    const todoPath= path.join(config.outputFolder, config.todoFile);
-    const todoOutput = JSON.stringify(cralwResult.todoRepos,null,2)
+    const todoPath = path.join(config.outputFolder, config.todoFile);
+    const todoOutput = JSON.stringify(cralwResult.todoRepos, null, 2)
     console.info("<<====>>>>><<<<<<<<>>>>>>><<<<<<====>>")
     console.info("we have still " + cralwResult.todoRepos.length + " repos todo");
 
     await fs.promises.writeFile(todoPath, todoOutput);
+}
+
+async function saveFishy(cralwResult: CrawlResult, config: FullConfiguration): Promise<void> {
+    if (!fs.existsSync(config.outputFolder)) {
+        fs.mkdirSync(config.outputFolder)
+    }
+    const fishyPath = path.join(config.outputFolder, config.fishyFile);
+    await fs.promises.writeFile(fishyPath, JSON.stringify(cralwResult.fhisyRepos, null, 3));
 }
--- src/repos.ts
+++ src/repos.ts
@@ -1,36 +1,60 @@
 import * as Octokit from '@octokit/rest';
 import * as moment from 'moment';
 
+import { FullConfiguration } from './index'
 import { RepositoryModel, Contributer, LanguageUsage, RepoIdentifier } from './model'
+import { getRateLimit } from './showlimit'
 
+const CALLS_PER_REPO = 4;
 
 export class CrawlResult {
     loadedRepos: Array<RepositoryModel>;
     todoRepos: Array<RepoIdentifier>;
-
+    fhisyRepos: Array<RepoIdentifier>;
 
     constructor(reposToRead: Array<RepoIdentifier>) {
-        this.loadedRepos = []
+        this.loadedRepos = [];
+        this.fhisyRepos = [];
         this.todoRepos = JSON.parse(JSON.stringify(reposToRead)); // "deep copy" see https://stackoverflow.com/a/23481096/1528488
-     }
+    }
 
     isFinished(): boolean {
         return this.todoRepos.length === 0;
     }
 
-    saveRepo(repoId: RepoIdentifier, loaded: RepositoryModel) {
+    saveRepo(repoId: RepoIdentifier, loaded: RepositoryModel): void {
         this.loadedRepos.push(loaded);
 
         console.log("remove repo" + repoString(repoId) + "from todo")
         this.todoRepos.forEach((item, index) => {
 
-            if (    (item.owner === repoId.owner)
-                     && (item.repository === repoId.repository)
-                ) {
+            if ((item.owner === repoId.owner)
+                && (item.repository === repoId.repository)
+            ) {
                 this.todoRepos.splice(index, 1);
-                
+
             }
         })
+    }
+
+    /* Some repos crash althoug they should work, we shold remove them from todo as well
+        eg; chriseppstein/ruby-freebase,  reinh/dm
+    */
+    saveAsFishy(currentId: RepoIdentifier): void{
+        this.fhisyRepos.push(currentId);
+        
+        this.todoRepos.forEach((item, index) => { 
+            if ((item.owner === currentId.owner)
+                && (item.repository === currentId.repository)
+            ) {
+                this.todoRepos.splice(index, 1);
+
+            }
+        })
+    }
+
+    hasFishyRepos():boolean{
+        return this.fhisyRepos.length !== 0;
     }
 }
 function repoString(repo: RepoIdentifier): string {
@@ -38,22 +62,35 @@ function repoString(repo: RepoIdentifier): string {
 }
 
 
-export async function crawlRepositories(repos: Array<RepoIdentifier>, gitApi: Octokit): Promise<CrawlResult> {
+export async function crawlRepositories(
+    repos: Array<RepoIdentifier>,
+    gitApi: Octokit,
+    config: FullConfiguration
+): Promise<CrawlResult> {
 
     let result = new CrawlResult(repos);
     console.info("Start readin repos")
-
+    let i = 1;
+    let max = repos.length
     for (let currentId of repos) {
 
-        console.log(`----> Process Reop (${currentId.owner}/${currentId.repository})`)
+        console.log(`----> Process Reop (${currentId.owner}/${currentId.repository}) #${i}/${max}`)
+        i +=1;
         try {
-            let loaded = await createModelFromResponse(currentId, gitApi)
+            let loaded = await createModelFromResponse(currentId, gitApi, config)
             result.saveRepo(currentId, loaded)
-        
+
         } catch (e) {
             console.error(e);
-            console.log("Will save current result")
-            return result;
+            const rateLimit = await getRateLimit(gitApi);
+            const remaining = rateLimit.resources.core.remaining;
+            if (remaining > CALLS_PER_REPO) {
+                console.log(`What ever happend...\n try to delete (${currentId.owner}/${currentId.repository}) from input. we still have ${remaining} calls`);
+                result.saveAsFishy(currentId)
+            } else {
+                console.log("Will save current result")
+                return result;
+            }
         }
 
     }
@@ -65,15 +102,16 @@ export async function crawlRepositories(repos: Array<RepoIdentifier>, gitApi: Oc
 /**
  * https://octokit.github.io/rest.js
  */
-export async function createModelFromResponse(repoId: RepoIdentifier, gitApi: Octokit): Promise<RepositoryModel> {
+export async function createModelFromResponse(
+    repoId: RepoIdentifier,
+    gitApi: Octokit,
+    config: FullConfiguration
+): Promise<RepositoryModel> {
+
 
     const response = (await gitApi.repos.get({ owner: repoId.owner, repo: repoId.repository }))
-    if (response.status == 403) {
-        console.info(`error happend why accesing repository ${repoId.owner}/${repoId.repository} : \n` + JSON.stringify(response.headers))
-        throw new Error('no more calls left')
-    }
-
     const repostoriy = response.data;
+
     let result: RepositoryModel = {
         id: repostoriy.id,
         owner: repostoriy.owner.login,
@@ -86,9 +124,9 @@ export async function createModelFromResponse(repoId: RepoIdentifier, gitApi: Oc
         updatedAt: repostoriy.updated_at,
         forkCount: repostoriy.forks_count,
         livetimeInDays: computeLifetime(repostoriy.created_at),
-        contributers: await createContributors(repoId, gitApi),
-        topics: await createTopics(repoId, gitApi),
-        usedLanguages: await createLanguages(repoId, gitApi)
+        contributers: await createContributors(repoId, gitApi, config),
+        topics: await createTopics(repoId, gitApi, config),
+        usedLanguages: await createLanguages(repoId, gitApi, config)
     }
     return result;
 }
@@ -97,7 +135,9 @@ function computeLifetime(created_at: string): number {
     const createdAt = moment(created_at);
     return moment().diff(createdAt, 'days');
 }
-async function createContributors(repostoriy: RepoIdentifier, gitApi: Octokit)
+async function createContributors(repostoriy: RepoIdentifier,
+    gitApi: Octokit,
+    config: FullConfiguration)
     : Promise<Array<Contributer>> {
 
     const response = await gitApi.repos.getContributorsStats({ owner: repostoriy.owner, repo: repostoriy.repository })
@@ -105,6 +145,10 @@ async function createContributors(repostoriy: RepoIdentifier, gitApi: Octokit)
     const contributerStats = response.data;
     //console.log("contributer:  " + JSON.stringify(contributerStats, null, 2))
     let result: Array<Contributer> = [];
+    if((contributerStats as any) === 'undefined'){
+        console.log('!! some thing funny happend: Repo has no contrib stats');
+        return [];
+    }
     for (let i = 0; i < contributerStats.length; i++) {
         let contrib = contributerStats[i];
         result.push({
@@ -112,22 +156,30 @@ async function createContributors(repostoriy: RepoIdentifier, gitApi: Octokit)
             totalSize: contrib.total
         })
     }
-
-    console.log(`Added ${result.length} contributors`);
+    if (config.shouldConsoleLog) {
+        console.log(`Added ${result.length} contributors`);
+    }
     return result;
 }
-async function createTopics(repo: RepoIdentifier, gitApi: Octokit)
-    : Promise<Array<string>> {
+async function createTopics(
+    repo: RepoIdentifier,
+    gitApi: Octokit,
+    config: FullConfiguration
+): Promise<Array<string>> {
     const response = await gitApi.repos.listTopics({ owner: repo.owner, repo: repo.repository })
     checkForLimitError(repo, response);
 
     const result: Array<string> = response.data.names
-    console.log(`Added Topics: ${JSON.stringify(result)}`)
-
+    if (config.shouldConsoleLog) {
+        console.log(`Added Topics: ${JSON.stringify(result)}`)
+    }
     return result;
 }
-async function createLanguages(repostoriy: RepoIdentifier, gitApi: Octokit)
-    : Promise<Array<LanguageUsage>> {
+async function createLanguages(
+    repostoriy: RepoIdentifier,
+    gitApi: Octokit,
+    config: FullConfiguration
+): Promise<Array<LanguageUsage>> {
     const response = await gitApi.repos.listLanguages({ owner: repostoriy.owner, repo: repostoriy.repository })
     checkForLimitError(repostoriy, response);
 
@@ -139,14 +191,16 @@ async function createLanguages(repostoriy: RepoIdentifier, gitApi: Octokit)
                 sizeInBytes: response.data[key]
             })
         })
-    console.log(`Added Languages: ${JSON.stringify(result)}`)
+    if (config.shouldConsoleLog) {
+        console.log(`Added Languages: ${JSON.stringify(result)}`)
+    }
 
     return result;
 }
 
 function checkForLimitError(repo: RepoIdentifier, response: any) {
     if (response.status == 403) {
-        console.info(`error happend why accesing contributers for${repo.owner}/${repo.repository} : \n` + JSON.stringify(response.headers))
+        console.info(`error happend while accessin repo ${repo.owner}/${repo.repository} : \n` + JSON.stringify(response.headers))
         throw new Error('no more calls left')
     }
 }
